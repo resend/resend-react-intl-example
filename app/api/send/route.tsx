ebeed3a5adf3ed26140ee34a2e79d72781cdc186
gabriel miranda <gabrielmfern@outlook.com>
2025-11-25 12:57:53 -0300

    lint

--- app/api/send/route.tsx
+++ app/api/send/route.tsx
@@ -1,20 +1,20 @@
-import Pricing from '@/emails/pricing';
 import { Resend } from 'resend';
+import Pricing from '@/emails/pricing';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
-  const body = await request.json() as {
-    locale: 'pt' | 'en'
+  const body = (await request.json()) as {
+    locale: 'pt' | 'en';
   };
   const response = await resend.emails.send({
     from: 'Acme <onboarding@resend.dev>',
     to: ['delivered@resend.dev'],
     subject: 'Pricing discount',
-    react: <Pricing locale={body.locale}/>
+    react: <Pricing locale={body.locale} />,
   });
 
   return Response.json(response, {
     status: response.error ? 500 : 200,
-  })
+  });
 }
